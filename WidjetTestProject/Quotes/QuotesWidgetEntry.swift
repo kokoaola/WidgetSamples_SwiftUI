fb7f93e540bbd8910cff180147ec1431ca5abfa2
koala panda <koakoa8823@protonmail.com>
2023-09-26 00:46:11 +0900

    コードの追加

---
+++ WidjetTestProject/Quotes/QuotesWidgetEntry.swift
@@ -0,0 +1,13 @@
+//
+//  QuoteWidgetEntry.swift
+//  SampleWidjetExtension
+//
+//  Created by koala panda on 2023/09/25.
+//
+
+import WidgetKit
+
+struct QuotesWidgetEntry: TimelineEntry{
+    var date: Date
+    var quote: Quote
+}
