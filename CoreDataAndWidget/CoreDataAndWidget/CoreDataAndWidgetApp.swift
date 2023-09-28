95a3415193e8ca10ee01d1b1332a24f46695bddf
koala panda <koakoa8823@protonmail.com>
2023-09-28 12:16:57 +0900

    プロジェクトの追加

---
+++ CoreDataAndWidget/CoreDataAndWidget/CoreDataAndWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataAndWidgetApp.swift
+//  CoreDataAndWidget
+//
+//  Created by koala panda on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataAndWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
